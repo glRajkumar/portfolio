642d4891cd4ceff64a3c409e7c8c9c278dc82ec0
Raj kumar <glrajv369@gmail.com>
2025-10-07 15:54:45 +0530

    feat: experience

--- components/home/data.ts
+++ components/home/data.ts
@@ -7,7 +7,8 @@ type eduT = {
 
 type descT = string | {
   title: string
-  description: string
+  listCls?: string
+  description?: string
   points: string[]
 }
 
@@ -16,7 +17,9 @@ type expT = {
   company: string
   timeline: string
   location: string
+  listCls?: string
   descriptions: descT[]
+  compDescription?: string
 }
 
 type projT = {
@@ -56,20 +59,24 @@ export const experiences: expT[] = [
     company: "HaiVE",
     timeline: "Feb 2024 - Jun 2025",
     location: "Hybrid - Chennai",
+    compDescription: "HaiVE is an AI-first SaaS company focused on research and developing innovative AI-driven products. I was responsible for end-to-end UI development across web, mobile, and desktop platforms, using React as the core technology. Contributed to 15+ projects from scratch, out of which two evolved into major products: Speakup, Nidum.AI.",
+    listCls: "list-decimal marker:font-medium",
     descriptions: [
       {
-        title: "HaiVE (Speakup)",
-        description: "",
+        title: "Speakup (Agent Studio)",
+        description: "Speakup is an AI Agent Studio platform that automates B2B customer interactions using AI-driven voice and chat assistants, positioned as a direct competitor to vapi.ai and bland.ai. It offers customizable AI workflows, multi-channel communication (voice and chat), and seamless third-party integrations.",
         points: [
           "Built an AI Agent Studio platform for automating B2B customer interactions using AI-powered voice and chat assistants — positioned as a direct competitor to vapi.ai and bland.ai.",
           "Developed the entire product from scratch on the frontend (NextJs and HonoJs), supporting features such as inbound/outbound phone call agents, intelligent chatbots, and customizable AI workflows tailored for enterprise clients.",
           "Integrated third-party platforms like Zendesk and Zoho Cliq via webhooks to enable seamless communication between AI agents and external systems.",
           "Led the development and maintenance of the web frontend, backend services (auth and user data), and chatbot functionality, ensuring a scalable and reliable experience across use cases.",
+          "Implemented a user-friendly interface for creating and managing AI agents, linking them to mobile numbers, deploying them for various interaction scenarios and customizing their behavior and chatbot interfaces.",
+          "Technologies used: NextJs, HonoJs, Tailwindcss, Zustand, React Query.",
         ]
       },
       {
-        title: "Nidum.AI",
-        description: "",
+        title: "Nidum.AI (Vibe Studio)",
+        description: "Nidum.AI is transforming the AI landscape by building a decentralized ecosystem that allows anyone to contribute computing power and earn rewards. Currently being acquired by BitsCrunch, a leading AI + Blockchain company in the crypto space.",
         points: [
           "Spearheaded the UI development of a decentralized AI ecosystem from the ground up, enabling users to access and interact with multiple AI models from providers like OpenAI, Claude, Groq, and others—all under one platform.",
           "Built and maintained web (React), mobile (React Native + Expo), and desktop (Electron) applications to ensure seamless cross-platform access.",
@@ -77,6 +84,7 @@ export const experiences: expT[] = [
           "Website features: Chat with various AI models, image uploads, and voice activity detection (VAD) support.",
           "Mobile app features: Chat with AI models, use local AI models, and upload images.",
           "Desktop app features: All web features, plus local model loading, document-based chat, and the ability for users to share their computation power by exposing models for use by others.",
+          "Technologies used: React, React Native + Expo, Electron, Tailwindcss, Zustand, React Query.",
         ]
       }
     ],
@@ -86,13 +94,15 @@ export const experiences: expT[] = [
     company: "Datalligence AI",
     timeline: "Jul 2023 - Feb 2024",
     location: "In-Office - Chennai",
+    compDescription: "Datalligence is an OKR and performance management platform built to drive team alignment, boost productivity, and accelerate execution within organizations.",
     descriptions: [
-      "Revamped OKR module UI with a sleek desig+n, enhancing user experience as well as developer experience.",
+      "Revamped OKR module UI with a sleek design, enhancing user experience as well as developer experience.",
       "Overhauled UI logics for a more intuitive and seamless interface with Antd design systems with Tailwindcss.",
       "Accelerated development speed by incorporating Tailwind, React Query, and Zustand.",
       "Successfully addressed and resolved major backend and frontend issues.",
       "Upgraded deprecated libraries, ensuring code integrity and future-proofing.",
       "worked on TypeORM with PostgreSQL on the backend.",
+      "Technologies used: React, Tailwindcss, React Query, Zustand, TypeORM, PostgreSQL.",
     ]
   },
   {
@@ -100,6 +110,7 @@ export const experiences: expT[] = [
     company: "Redink.app",
     timeline: "Feb 2022 - Jun 2023",
     location: "Remote",
+    compDescription: "Redink is a leading LMS platform for K-12 Schools",
     descriptions: [
       "Successfully developed Redink classroom v2 (Teacher and Admin roles), showcasing my ability to create effective and user-friendly modern website. (React ecosystem with tailwindcss)",
       "I proactively addressed critical bugs and introduced new features, resulting in 78 pull requests and showcasing my commitment to continuous improvement in our current web app.",
@@ -108,12 +119,12 @@ export const experiences: expT[] = [
       "Managed various client projects, highlighting my diverse expertise in delivering tailored solutions.",
       {
         title: "Notable works",
-        description: "",
+        listCls: "list-decimal marker:font-medium",
         points: [
-          "1) Redink Classroom website - developed new website for our company in Nextjs. link - https://redink.app",
-          "2) Upfunda website (previously known as YouthInYou) - developed in Nextjs. link - https://upfunda.academy",
-          "3) Truseve website (home, /microsoft, /hardware pages alone) - developed in Nextjs. link - https://www.truseve.com",
-          "4) Skillaura web platform (Admin and Student roles) - developed in Reactjs with firebase authentication. Except home page, other pages not mobile optimized. link - https://skillaura.com"
+          "Redink Classroom website - developed new website for our company in Nextjs. link - https://redink.app",
+          "Upfunda website (previously known as YouthInYou) - developed in Nextjs. link - https://upfunda.academy",
+          "Truseve website (home, /microsoft, /hardware pages alone) - developed in Nextjs. link - https://www.truseve.com",
+          "Skillaura web platform (Admin and Student roles) - developed in Reactjs with firebase authentication. Except home page, other pages not mobile optimized. link - https://skillaura.com"
         ]
       },
       "Overall, I excelled as a single developer, proving my capacity to take ownership of projects and deliver high-quality results independently. Teck stack utilized - React ecosystem, Nextjs, Tailwindcss.",
@@ -124,10 +135,10 @@ export const experiences: expT[] = [
     company: "Teknuance Info Solutions",
     timeline: "Nov 2020 - Feb 2022",
     location: "Remote",
+    compDescription: "Teknuance Info Solutions is a product and research-driven organization delivering innovative solutions to complex challenges across industries",
     descriptions: [
       {
         title: "Full Time",
-        description: "",
         points: [
           "Designed and Developed Sabhae Bot which processed, analyzed and rendered dynamic complex data as simple user interface in individual components. ",
           "Worked closely with Natural Language Processing and Machine learning Teams, planned and designed the flow of the Bot. ",
@@ -138,7 +149,6 @@ export const experiences: expT[] = [
       },
       {
         title: "Internship",
-        description: "",
         points: [
           "Worked in basic tasks like creating simple webpages using react and redux.",
           "Created Simple Chat application using MERN with Socket.io.",
--- components/home/experiences.tsx
+++ components/home/experiences.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils";
 import { experiences } from "./data";
 
 function Experiences() {
@@ -8,41 +9,50 @@ function Experiences() {
       {
         experiences.map((ex, i) => (
           <div key={ex.title} className="revealing mb-8 rounded-2xl border relative isolate">
-            <span className={`timeline-line w-px ${i === experiences.length - 1 ? "[--h:90%] md:[--h:80%]" : ""} absolute top-12 -left-px bg-gradient-to-b from-border via-border to-teal-300 rounded-full`}></span>
+            <span className={`timeline-line w-px ${i === experiences.length - 1 ? "[--h:90%] md:[--h:80%]" : ""} absolute top-12 -left-px bg-gradient-to-b from-border via-border to-teal-600 dark:to-teal-300 rounded-full`}></span>
 
-            <div className="df justify-between gap-4 p-4 pl-7 sticky top-20 rounded-2xl bg-secondary">
+            <div className="df justify-between flex-wrap sm:gap-4 p-4 pl-7 sticky top-20 rounded-2xl bg-secondary border shadow dark:shadow-white/10">
               <span className="size-5 absolute top-1/2 -left-[11px] -translate-y-1/2 bg-border dark:bg-secondary border border-accent-foreground/70 rounded-full z-[1]"></span>
 
-              <div className="flex-1">
+              <div className="sm:flex-1 text-sm sm:text-base font-medium">
                 <p>{ex.title}</p>
                 <p>{ex.company}</p>
               </div>
 
-              <div>
+              <div className="text-xs sm:text-sm">
                 <p>{ex.timeline}</p>
                 <p>{ex.location}</p>
               </div>
             </div>
 
-            <div className="p-4">
+            <div className="mb-2 sm:mb-4 px-4 pt-2.5 sm:pt-4 text-[13px] sm:text-[15px]">
+              {ex.compDescription}
+            </div>
+
+            <ul className={cn("px-2 sm:px-4 pb-4 text-[13px] sm:text-[15px] list-disc list-inside marker:text-secondary-foreground/70", ex.listCls)}>
               {ex.descriptions.map((d, j) => {
                 if (typeof d === "string") {
                   return (
-                    <p className="mb-1" key={j}>{d}</p>
+                    <li className="mb-1" key={j}>{d}</li>
                   )
                 }
+
                 return (
-                  <div className="pl-4 mb-4" key={j}>
-                    <h6>{d.title}</h6>
-                    {
-                      d.points.map((p, i) => (
-                        <p className="mb-1 pl-4" key={i}>{p}</p>
-                      ))
-                    }
-                  </div>
+                  <li className="my-2 sm:my-4" key={j}>
+                    <h6 className="inline-block font-semibold">{d.title}</h6>
+                    {d.description && <p className="mb-2">{d.description}</p>}
+
+                    <ul className={cn("pl-7 sm:pl-10 list-disc marker:text-secondary-foreground/70", d.listCls)}>
+                      {
+                        d.points.map((p, i) => (
+                          <li className="mb-1" key={i}>{p}</li>
+                        ))
+                      }
+                    </ul>
+                  </li>
                 )
               })}
-            </div>
+            </ul>
           </div>
         ))
       }
