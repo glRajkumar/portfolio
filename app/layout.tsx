7da798bc20ed4e7dc203873c94dc04d8b45b46aa
Raj kumar <glrajv369@gmail.com>
2025-10-10 16:04:20 +0530

    seo base

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,35 @@ import "./globals.css";
 
 import { ThemeProvider } from "@/components/theme/provider";
 
+const config = {
+  url: "https://glrk.dev",
+  name: "Raj kumar",
+  title: "Raj kumar | Full Stack Web Developer",
+  description: "Raj Kumar is a Full Stack Web Developer specializing in React and Node.js ecosystem.",
+  appName: "glrk",
+}
+
 export const metadata: Metadata = {
-  title: "Portfolio - Raj kumar",
-  description: "",
+  title: config.title,
+  description: config.description,
+  keywords: ["Raj kumar portfolio", "Raj kumar full stack web developer", "Raj kumar full stack developer", "Raj kumar MERN stack developer", "React dev", "Node.js dev", "Next.js dev", "MERN stack dev", "Full stack dev", "Web developer"],
+  authors: [{ name: config.name, url: config.url }],
+  applicationName: config.appName,
+  openGraph: {
+    title: config.title,
+    description: config.description,
+    url: config.url,
+    locale: "en_IN",
+    type: "website",
+    siteName: config.appName,
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: config.title,
+    description: config.description,
+    site: `@${config.appName}`,
+    creator: "@glrajkumar01",
+  },
 }
 
 function RootLayout({ children }: readOnlyChildren) {
