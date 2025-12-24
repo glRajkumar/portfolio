39a7b281a9149ec83b672da61087cc11cf2fccb0
Raj kumar <glrajv369@gmail.com>
2025-12-24 11:25:09 +0530

    feat: /blog/git-clone-with-ssh

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,9 +1,16 @@
-import defaultMdxComponents from 'fumadocs-ui/mdx';
+import * as AccordionComps from 'fumadocs-ui/components/accordion';
+import * as StepsComps from 'fumadocs-ui/components/steps';
+import * as TabsComps from 'fumadocs-ui/components/tabs';
+import defaultComps from 'fumadocs-ui/mdx';
+
 import type { MDXComponents } from 'mdx/types';
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
-    ...defaultMdxComponents,
+    ...defaultComps,
+    ...AccordionComps,
+    ...StepsComps,
+    ...TabsComps,
     ...components,
   };
 }
